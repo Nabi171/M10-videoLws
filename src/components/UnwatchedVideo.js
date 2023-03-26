dd40bec10dfa13d7626d12d11c3be30037179d69
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-26 22:30:10 +0600

    changes in code

--- src/components/UnwatchedVideo.js
+++ src/components/UnwatchedVideo.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useSelector, shallowEqual } from "react-redux";
+import VideoItem from "./VideoItem";
+import { selectUnWatchedVideos } from '../features/videos/videoSelector';
+// import { selectWatchedVideos } from "../features/videos/videoSelector";
 
 const UnwatchedVideo = () => {
+    const unwatchedVideos = useSelector(selectUnWatchedVideos);
+
+    console.log("[UnWatchedVideos] renders");
+
     return (
         <div>
-
+            <ul className="divide-y divide-slate-200">
+                {unwatchedVideos.map((video) => (
+                    <VideoItem key={video.id} video={video} />
+                ))}
+            </ul>
         </div>
     );
-};
-
+}
 export default UnwatchedVideo;
