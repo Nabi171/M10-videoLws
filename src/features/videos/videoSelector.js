aae3b9bc758d2cc0d670d8fc67d14cef63b71b89
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-26 23:07:24 +0600

    changes in code

--- src/features/videos/videoSelector.js
+++ src/features/videos/videoSelector.js
@@ -1,5 +1,15 @@
+import { createSelector } from "reselect/*";
+
+
 export const selectAllVideos = (state) => state.videos.videos;
 export const selectWatchedVideos = (state) =>
     state.videos.videos.filter((v) => v.watched === true);
 export const selectUnWatchedVideos = (state) =>
     state.videos.videos.filter((v) => v.watched === false);
+
+export const selectMemoizedWatchedVideos = createSelector(
+    selectAllVideos,
+    (allVideos) => {
+        return allVideos.filter((v) => v.watched === true);
+    }
+)
